cc84723cdc94898e75049629256b90de2ef4a1f8
Pignataro67 <Pignataro67@gmail.com>
2024-01-20 19:11:49 -0800

    JS

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 
 let load = 0
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
