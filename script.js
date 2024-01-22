5b631faa2102b9af41533cc95e9736636f6a8f76
Pignataro67 <Pignataro67@gmail.com>
2024-01-21 18:06:49 -0800

    JS

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ let int = setInterval(blurring, 30
         if (load > 99) {
           clearInterval(int)
         }
+
+        loadText.innerText = `${load}%`
+        loadText.style.opacity = scale(load, 0, 100, 1, 0)
+        bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
     }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
